f04a22b05d5ecd9bba1d1d41bc163587afe96b6f
Ehmad <justehmadsaeed@gmail.com>
2021-06-12 22:42:07 +0500

    📦 NEW: ADD data of api-integration & form-validation

--- lib/ASSIGNMENT_DATA.dart
+++ lib/ASSIGNMENT_DATA.dart
@@ -1,16 +1,32 @@
 const ASSIGNMENT_DATA = [
   {
+    'title': 'Web Api Integration',
+    'widgets': ['TextField', 'Elevated Button', 'Drawer', 'Alert Dialog'],
+    'description':
+        'A Flutter application with proper navigation, routes handling, API data fetching and posting.',
+    'image_url': 'assets/api-integration.png',
+    'video_url': 'assets/api-integration.mp4'
+  },
+  {
     'title': 'Form Validation',
     'widgets': ['TextField', 'Elevated Button', 'SnackBar'],
     'description':
-        'The Form shows multiple input methods and shows any invalidations on submitting the data.',
-    'video_url': 'assets/flutter-demo.mp4'
+        'The Form shows multiple input methods and any invalidations on submitting the data like email format, empty fields and strong passwords.',
+    'image_url': 'assets/form-validation.png',
+    'video_url': 'assets/form-validation.mp4'
   },
   {
-    'title': 'Form Validation 2',
-    'widgets': ['TextField', 'Elevated Button', 'SnackBar'],
+    'title': 'Tab Bars',
+    'widgets': [
+      'TabController',
+      'TabBar',
+      'DropDown',
+      'DatePicker',
+      'Pop-up Button'
+    ],
     'description':
-        'The Form shows multiple input methods and shows any invalidations on submitting the data.',
-    'video_url': 'assets/flutter-demo.mp4'
-  }
+        'The app contains the implementation of different kinds of widgets including drawers, tabs, and routes.',
+    'image_url': 'assets/form-validation.png',
+    'video_url': 'assets/form-validation.mp4'
+  },
 ];
